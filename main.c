054a6631af2d6b9d1fe4ff9b83f626c420d3ae9b
Sean E <seanmembley@proton.me>
2025-11-03 17:03:21 -0500

    chore: cleaning up Moving around functions to places that make more sense.

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-#include "src/dirs.c"
 #include "src/includes.h"
 #include "src/makenote.c"
 #include <linux/limits.h>
@@ -6,6 +5,7 @@
 
 void mkentry(FILE *note);
 void initcheck(void);
+int initdirs();
 
 int main(int argc, char *argv[]) {
   bool daily = true;
@@ -62,4 +62,25 @@ void initcheck(void) {
   }
   closedir(d);
   initdirs();
+}
+
+int initdirs() {
+  if(mkdir("Diary", 0744) == -1) {
+    fprintf(stderr, "Directory creation failed: %s\n", strerror(errno));
+    return 1;
+  }
+  
+  for(int i = 0; i < sizeof(months)/sizeof(months[0]); i++) {
+    char dir[32];
+    snprintf(dir, sizeof(dir), "Diary/%s", months[i]);
+    if(mkdir(dir, 0744) == -1) {
+      if(errno == EEXIST) {
+        fprintf(stderr, "Directory creation failed, \"%s\" already exists: %s\n", dir, strerror(errno));
+      } else {
+        fprintf(stderr, "\"mkdir\" failed: %s\n", strerror(errno));
+      }
+    }
+  }
+  
+  return 0;
 }
--- qol/run.sh
+++ qol/run.sh
@@ -2,6 +2,6 @@
 
 ./qol/test.sh
 printf '\nHere are the contents of thoughts.typ\n\"\n'
-cat Diary/10.October/thoughts:2.typ
+cat Diary/10.October/thoughts:3.typ
 printf '\"\n'
 rm main
