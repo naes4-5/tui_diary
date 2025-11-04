c7d272cf131b5bb10e2e149201ec96e6adb12b32
Sean E <seanmembley@proton.me>
2025-11-03 21:38:35 -0500

    feat: title Now a title can be passed directly after the '-t' flag

--- main.c
+++ main.c
@@ -9,33 +9,44 @@ int initdirs();
 
 int main(int argc, char *argv[]) {
   bool daily = false;
+  char *title = malloc(16);
+  memcpy(title, "thoughts", sizeof("thoughts"));
   int in = 0;
-  char opts[argc-1];
-  for(int i = 1; i < argc; i++) {
+  char opts[argc - 1];
+  for (int i = 1; i < argc; i++) {
     if (*argv[i] == '-' && strlen(argv[i]) > 1) {
-      char arg = argv[i][1];
-      if (arg == 'd') {
+      switch (argv[i][1]) {
+      case 'd':
         daily = true;
-      } else if (arg == 'p') {
-        printf("for a project (to be implemented)\n");
+        break;
+      case 'p':
+        printf("More options for a project...ig\n");
+        break;
+      case 't':
+        if (strlen(argv[++i]) >= 16) {
+          printf("title is too long, defaulting to 'thoughts'\n");
+          break;
+        }
+        memcpy(title, argv[i], strlen(argv[i])+1);
+        break;
+      default:
+        printf("Flag '-%c' not supported\n", argv[i][1]);
       }
     }
   }
   initcheck();
-  FILE *note = mknote(daily, "thoughts");
+  FILE *note = mknote(daily, title);
   if (!note) {
     fprintf(stderr, "Error in opening, aborting.\n");
     return 1;
   }
-  mkentry(note, argv[argc-1]);
-  fclose(note);
+  mkentry(note, argv[argc - 1]);
 
+  fclose(note);
   return 0;
 }
 
-void mkentry(FILE *note, const char *entry) {
-  fprintf(note, "%s\n", entry);
-}
+void mkentry(FILE *note, const char *entry) { fprintf(note, "%s\n", entry); }
 
 void initcheck(void) {
   char path[PATH_MAX];
@@ -62,22 +73,24 @@ void initcheck(void) {
 }
 
 int initdirs() {
-  if(mkdir("Diary", 0744) == -1) {
+  if (mkdir("Diary", 0744) == -1) {
     fprintf(stderr, "Directory creation failed: %s\n", strerror(errno));
     return 1;
   }
-  
-  for(int i = 0; i < sizeof(months)/sizeof(months[0]); i++) {
+
+  for (int i = 0; i < sizeof(months) / sizeof(months[0]); i++) {
     char dir[32];
     snprintf(dir, sizeof(dir), "Diary/%s", months[i]);
-    if(mkdir(dir, 0744) == -1) {
-      if(errno == EEXIST) {
-        fprintf(stderr, "Directory creation failed, \"%s\" already exists: %s\n", dir, strerror(errno));
+    if (mkdir(dir, 0744) == -1) {
+      if (errno == EEXIST) {
+        fprintf(stderr,
+                "Directory creation failed, \"%s\" already exists: %s\n", dir,
+                strerror(errno));
       } else {
         fprintf(stderr, "\"mkdir\" failed: %s\n", strerror(errno));
       }
     }
   }
-  
+
   return 0;
 }
