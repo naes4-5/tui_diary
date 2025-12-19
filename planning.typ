a39ffb19f569b3c4326869ba583d1af57afb1bd7
Sean E <seanmembley@proton.me>
2025-12-19 17:24:28 -0500

    feat: full implementation of write with project support The full working implementation of the write command with support for the -p flag for projects.

--- planning.typ
+++ planning.typ
@@ -18,6 +18,11 @@ direy write "started implementing feature {x}"
 Makes a new note with the current date with the inputted text \ \
 
 ```bash
+diery write -a "amending to previous message"
+```
+Amends the message to the last note from a normal write command \ \
+
+```bash
 direy write -p "Goal: Help with context switching"
 ```
 Goals/notes for the entire project \ \
--- src/main.c
+++ src/main.c
@@ -1,81 +1,69 @@
 #include "../includes/includes.h"
-#include "makenote.c"
-#include "findpaths.c"
 #include "cltools.c"
-
-DIR *initdir();
+#include "findpaths.c"
+#include "makenote.c"
+#include <linux/limits.h>
 
 int main(int argc, char *argv[]) {
-    char *message;
-    char *project = get_project_name();
-    if (!project) {
-        return exit_error("No git repository found, aborting", NOREPO);
+    const char *homepath = getenv("HOME");
+    if (!homepath) {
+        return exit_error("Error opening home directory, aborting.\nTryagain.",
+                          BADDIR);
     }
-
-    DIR *projectdir = initdir();
-    if (!projectdir) {
-        return exit_error("Directory faield to open, aborting.\nTry again.",
+    char dierypath[PATH_MAX];
+    int written =
+        snprintf(dierypath, sizeof(dierypath), "%s/.diery/", homepath);
+    if (written > sizeof(dierypath) || written < 0) {
+        return exit_error("Error getting path to '~/.diery'.\nTry again.",
                           BADDIR);
     }
 
-    operation operation = get_operation(argc, argv);
-    if (operation == NOOP) {
-        closedir(projectdir);
+    char *projectname = get_project_name();
+    if (!projectname) {
+        return exit_error("No git repository found, aborting", NOREPO);
+    }
+
+    char projectpath[PATH_MAX];
+    written = snprintf(projectpath, sizeof(projectpath), "%s%s", dierypath,
+                       projectname);
+    if (written > sizeof(dierypath) || written < 0) {
+        return exit_error("Error getting path to '~/.diery'.\nTry again.",
+                          BADDIR);
+    }
+
+    char *configpath = config_path();
+    if (!configpath) {
+        return exit_error("Failed to open configuration", BADDIR);
+    }
+
+    operation op = get_operation(argc, argv);
+    if (op == NOOP) {
         return exit_error("Must have an operation:\nread\nwrite", BADOPERATION);
     }
 
-    note_t notelevel = get_arg(argv[2]);
+    note_t notelevel = get_note_type(argc, argv);
     if (notelevel == INVALID) {
-        closedir(projectdir);
         return exit_error("Flag must be valid:\n-p -> project level note",
                           BADFLAG);
     }
 
-    if (operation == READ) {
-        printf("Successfully read from %s\n", project);
-    } else if (operation == WRITE) {
-        printf("Successfully wrote to %s ", project);
-        if (notelevel == PROJECT) {
-            printf("as a project note!");
-        }
-        printf("\n");
+    // Need the DIR for reading files
+    DIR *projectdir = get_project_dir(homepath, projectname);
+    if (!projectdir) {
+        return exit_error("Directory faileld to open, aborting.\nTry again.",
+                          BADDIR);
     }
 
-    char *path = config_path();
-    if (!path) {
-        closedir(projectdir);
-        return exit_error("Failed to open configuration", BADDIR);
+    if (op == READ) {
+        printf("Successfully read from %s\n", projectname);
+    } else if (op == WRITE) {
+        int noteexit =
+            make_note(projectpath, projectname, argv[argc - 1], notelevel);
+        if (noteexit) {
+            return exit_error("could not make note", noteexit);
+        }
     }
-    printf("path is %s\n", path);
 
     closedir(projectdir);
     return 0;
-}
-
-DIR *initdir() {
-    static char path[PATH_MAX];
-
-    // Find the relevant folder
-    const char *homePath = getenv("HOME");
-    if (!homePath) {
-        return NULL;
-    }
-
-    int written = snprintf(path, sizeof(path), "%s/.diery", homePath);
-    if (written > sizeof(path) || written < 0) {
-        return NULL;
-    }
-
-    if (mkdir(path, 0755) == -1 && errno != EEXIST) {
-        perror("Error making ~/.diery");
-        return NULL;
-    }
-
-    DIR *d;
-    struct dirent *entry;
-    d = opendir(path);
-    if (!d) {
-        perror("Error opening ~/.diery");
-    }
-    return d;
 }
